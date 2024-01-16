b6071feac2263a9d43cd5311720a364736ca74ea
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2024-01-16 14:47:46 +0100

    create object

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animel = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animel);
+var obj1 = {
+    name: "Anton",
+    age: 15,
+    married: true,
+};
+console.log(obj1);
--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
 let animel: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animel);
+
+let obj1: object = {
+  name: "Anton",
+  age: 15,
+  married: true,
+};
+console.log(obj1);
